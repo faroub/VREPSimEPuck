53e70f079cae4a25cc9daf8a134c93c5d4b0bd6f
Farid Oubbati <f.oubbati@yahoo.f>
2018-10-22 17:31:20 +0100

    changed .gitignore

--- src-matlab/normalizeAngle.m
+++ src-matlab/normalizeAngle.m
@@ -1,3 +1,3 @@
 function normalizedAngle = normalizeAngle( angle )
-% normalizes an given angle to a range between -pi ... pi
+% normalizes a given angle to a range between -pi ... pi
 normalizedAngle = mod( angle + pi, 2*pi ) - pi;
